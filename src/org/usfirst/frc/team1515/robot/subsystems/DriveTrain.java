334c601250f9556a3b0b715f87549000fdad0979
Kian <kianv2002@gmail.com>
2018-12-08 11:25:39 -0800

    Tele-op mode fully functional

--- src/org/usfirst/frc/team1515/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team1515/robot/subsystems/DriveTrain.java
@@ -45,13 +45,13 @@ public class DriveTrain extends Subsystem {
 	
 	public void sendingForwardSpeed(double speed) {
 //		speed of going forward and backwards
-		SmartDashboard.putNumber("F Speed (Theoretical)", speed);
+		SmartDashboard.putNumber("F Speed (Theoretical):", speed);
 	}
 	
 	public void sendingRotationalSpeed(double leftSpeed, double rightSpeed) {
 //		speed of turning left and right
-		SmartDashboard.putNumber("L Speed (Theoretical)", leftSpeed);
-		SmartDashboard.putNumber("R Speed (Theoretical)", rightSpeed);
+		SmartDashboard.putNumber("L Speed (Theoretical):", leftSpeed);
+		SmartDashboard.putNumber("R Speed (Theoretical):", rightSpeed);
 	}
 	
 	public void setSpeeds(double leftSpeed, double rightSpeed) {
