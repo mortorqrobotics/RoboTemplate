a543db5452bb69aa52601f28e7ecb40349fd88af
Kian <kianv2002@gmail.com>
2018-12-17 18:37:19 -0800

    Update DriveTrain.java

--- src/org/usfirst/frc/team1515/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team1515/robot/subsystems/DriveTrain.java
@@ -77,13 +77,7 @@ public class DriveTrain extends Subsystem {
 	public void drive() {
 		double forward = Robot.driveStick.getRawAxis(Controls.Y_AXIS);
 		double twist = -Robot.driveStick.getRawAxis(Controls.TWIST);
-		double throttle = Robot.driveStick.getRawAxis(Controls.THROTTLE);
-		
-//		test new drift code below 
-
-//		drift #1
-		double drift = Robot.driveStick.getRawAxis(Controls.DRIFT);
-		
+		double throttle = Robot.driveStick.getRawAxis(Controls.THROTTLE);	
 		double turnSpeed = Robot.driveStick.getRawAxis(Controls.TURN_SPEED);
 		
 		forward = Math.abs(forward) > DEADBAND_FORWARD ? forward : 0;
@@ -91,15 +85,9 @@ public class DriveTrain extends Subsystem {
 		
 		turnSpeed = (1 + turnSpeed)/2;
 		throttle = (throttle - 1)/2;
-		
-//		dirft #2 (could be -1 depending on if robot accelerates or decelerates)
-		drift = (1 + drift)/2;
 			
 		forward *= throttle;
-		
-//		drift #3 (might have to use if/else statement if (forward *= throttle;) & (forward *= drift;) don't work together
 		forward *= drift;
-		
 		twist *= turnSpeed;
 		
 		double y = Math.abs(forward);
@@ -113,8 +101,6 @@ public class DriveTrain extends Subsystem {
 			left *= -1;
 			right *= -1;
 			twist *= -1;
-//			drift code #2
-			drift *= -1;
 		}
 		if (twist > 0) {
 			double temp = left;
